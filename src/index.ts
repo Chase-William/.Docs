5ca896f6c249bccff9bbf5b8e312ec15eb0358df
cclcr21 <cclcr21@rit.edu>
2022-05-21 12:56:36 -0400

    abs path based, but functional

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ const spawn = import('child_process')
 
 spawn.then((proc) => {
   // proc.execSync('start \"C:\\Dev\\Sharpocs\\src\\Docsharp\\bin\\Debug\\net5.0\"')
-  proc.execFileSync('C:\\Dev\\Sharpocs\\src\\Docsharp\\bin\\Debug\\net5.0\\Docsharp.exe')  
+  proc.execFileSync(
+    'C:\\Dev\\Charp\\vendor\\Charp.Core\\src\\Charp\\bin\\Debug\\net5.0\\Charp.exe', [ 
+      'C:\\Dev\\Charp\\vendor\\Charp.Core\\test\\Charp.Test.Data\\bin\\Debug\\net5.0\\Charp.Test.Data.dll', 
+      'C:\\Dev\\Charp\\vendor\\Charp.Core\\test\\Charp.Test.Data\\bin\\Debug\\net5.0\\Charp.Test.Data.xml' 
+    ])
 }, (failed) => {
   console.log(failed)
 }).finally(() => {
